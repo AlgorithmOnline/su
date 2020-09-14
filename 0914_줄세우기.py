6799a20739552afa9f110a6be566cb1a3b5146e7
sdkfhdquf <47805080+sdkfhdquf@users.noreply.github.com>
2020-09-14 23:23:59 +0900

    백준_줄세우기

--- 0914_줄세우기.py
+++ 0914_줄세우기.py
@@ -3,7 +3,7 @@
 import sys
 from collections import deque
 sys.stdin = open("input.txt","r")
-n,m=map(int,input().split()) #노드개수, 간선연결
+n,m=map(int,input().split()) 
 graph=[[] for _ in range(n+1)]
 indegree=[0]*(n+1) #진입차수
 q=deque()
@@ -15,17 +15,15 @@ for _ in range(m):
 
 def topologysort():
     for i in range(1,n+1):
-        #진입차수가 0인것 큐에 삽입
         if indegree[i]==0:
             q.append(i)
 
     while q:
-        cur=q.popleft() #큐에서 뺄때마다 연관된 간선 없애주기
+        cur=q.popleft() 
         result.append(cur)
-        #cur과 연결되어 있는 진입차수를 1씩 뺀다
         for i in graph[cur]:
             indegree[i]-=1
-            if indegree[i]==0:#진입차수가 0인것 계속해서 큐에 넣기
+            if indegree[i]==0:
                 q.append(i)
 
 topologysort()
