76686be93eac515b2b6847f66c9d2fe1c9e71272
sdkfhdquf <47805080+sdkfhdquf@users.noreply.github.com>
2020-10-20 23:40:20 +0900

    프로그래머스_숨박꼭질6

---
+++ 1020_숨박꼭질6.py
@@ -0,0 +1,18 @@
+#숨바꼭질 6
+#https://www.acmicpc.net/problem/17087
+import sys
+sys.stdin = open("input.txt","r")
+input=sys.stdin.readline
+n,s=map(int,input().split())
+brother=list(map(int,input().split()))
+dis=[]
+for i in range(n): 
+    dis.append(abs(s-brother[i]))
+res=dis[0]
+def gcd(a,b): 
+    while a>0:
+        a,b=b%a,a
+    return b
+for i in range(1,n):
+    res=gcd(res,dis[i]) 
+print(res)
