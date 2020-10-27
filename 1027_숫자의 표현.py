8de5d55824f74b70482ae2d8bcf6a4b875ed0d1f
sdkfhdquf <47805080+sdkfhdquf@users.noreply.github.com>
2020-10-27 15:09:01 +0900

    프로그래머스_숫자의 표현

--- 1027_숫자의 표현.py
+++ 1027_숫자의 표현.py
@@ -1,12 +1,12 @@
 def solution(n):
     cnt=0
-    for i in range(1,n+1): # 1~n까지 차례대로 기준 설정 
+    for i in range(1,n+1):  
         sum=0
-        for j in range(i,n+1): #기준점부터
+        for j in range(i,n+1):
             sum+=j
-            if sum==n: #합이 n과 같으면 
+            if sum==n: 
                 cnt+=1
                 break
-            elif sum>n: #합이 n보다 크면 더 할 필요없음 
+            elif sum>n: 
                 break
     return cnt
