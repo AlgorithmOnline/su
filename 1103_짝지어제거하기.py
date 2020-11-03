fd4d9666a77811cedd34a973834da610b5fcb71a
sdkfhdquf <47805080+sdkfhdquf@users.noreply.github.com>
2020-11-03 18:41:58 +0900

    프로그래머스_짝지어제거하기

---
+++ 1103_짝지어제거하기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = 0
+
+    stack=[]
+    for i in range(len(s)):
+        if len(stack)==0 or stack[-1]!=s[i]:
+            stack.append(s[i])
+        else:
+            stack.pop()
+    if stack:
+        return 0
+    else:
+        return 1
